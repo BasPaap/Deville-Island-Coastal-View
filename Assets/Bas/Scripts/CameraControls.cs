e4d4a6b18bb24c47c405c8dd47de327bb3b8929e
Bas Paap <baspaap@live.nl>
2024-08-26 19:20:25 +0200

    Add scroll wheel controls.

--- Assets/Bas/Scripts/CameraControls.cs
+++ Assets/Bas/Scripts/CameraControls.cs
@@ -5,6 +5,7 @@ public class CameraControls : MonoBehaviour
     [SerializeField] private float minYAngle = -45f;
     [SerializeField] private float maxYAngle = 45f;
     [SerializeField, Range(0,10f)] private float rotationSpeed = 1.0f;
+    [SerializeField, Range(-10, 10f)] private float mouseRotationSpeed = 1.0f;
 
     private Quaternion minRotation;
     private Quaternion maxRotation;
@@ -17,6 +18,17 @@ public class CameraControls : MonoBehaviour
 
     void Update()
     {
+        var input = Input.GetAxis("Horizontal");
+
+        if (Input.mouseScrollDelta.y > 0 )
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, minRotation, Mathf.Abs(Input.mouseScrollDelta.y) * mouseRotationSpeed * Time.deltaTime);
+        }
+        else if (Input.mouseScrollDelta.y < 0 )
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, maxRotation, Mathf.Abs(Input.mouseScrollDelta.y) * mouseRotationSpeed * Time.deltaTime);
+        }
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.rotation = Quaternion.RotateTowards(transform.rotation, minRotation, rotationSpeed * Time.deltaTime);
